e3ea8f37d23ed82c4a8c1020654eb7d52c32381c
B12 <blessenstanley12@gmail.com>
2025-05-10 00:56:43 +0530

    Fix player symbol assignment for Tic Tac Toe

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var orginBoard;
-const huPlayer = "0";
-const aiPlayer = "X";
+const huPlayer = "X";
+const aiPlayer = "0";
 const winCombos = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8],
   [0, 3, 6], [1, 4, 7], [2, 5, 8],
